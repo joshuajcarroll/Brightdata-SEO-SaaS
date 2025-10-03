a730ce72c5876b12713d8fedfef3016796c80c32
Joshua Carroll <carrolljosh1976@gmail.com>
2025-10-03 15:25:13 -0400

    added pricing page to app

---
+++ app/pricing/page.tsx
@@ -0,0 +1,11 @@
+import { PricingTable } from "@clerk/nextjs";
+
+function PricingPage() {
+  return (
+    <div>
+      <PricingTable newSubscriptionRedirectUrl="/dashboard" />
+    </div>
+  );
+}
+
+export default PricingPage;
